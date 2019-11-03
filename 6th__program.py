79162182f251d2edcdde6168152e6f3f67df1c27
= <=>
2019-11-03 12:01:55 +0500

    Uploaded 6th Program

---
+++ 6th__program.py
@@ -0,0 +1,10 @@
+'''
+
+Write a python program which takes two inputs from user and print them
+addition
+
+'''
+Number1 = int(input('Enter First Number : '))
+Number2 = int(input('Enter Second Number : '))
+print('Addition = ',Number1+Number2)
+
