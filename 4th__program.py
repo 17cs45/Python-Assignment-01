096e90db1d86d2434624e6d67a85baf7f799f49e
= <=>
2019-11-03 12:01:34 +0500

    Uploaded 4th Program

---
+++ 4th__program.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program which accepts the radius of a circle from the user
+
+and compute the area.
+'''
+Radius = int(input('Enter Radius of Circle : '))
+PI = 3.1421
+Area = 2*PI*Radius**2
+print('Area of Circle = ',round(Area,3)) 
